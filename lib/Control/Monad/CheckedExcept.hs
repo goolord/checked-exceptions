e1e4357d43c0344025665bdc890f19a3841cd55e
Zachary Churchill <zacharyachurchill@gmail.com>
2020-09-19 16:52:35 -0400

    init commit

---
+++ lib/Control/Monad/CheckedExcept.hs
@@ -0,0 +1,153 @@
+{-# LANGUAGE
+    KindSignatures
+  , TypeFamilies
+  , DataKinds
+  , TypeOperators
+  , UndecidableInstances
+  , GADTs
+  , TypeApplications
+  , ScopedTypeVariables
+  , RankNTypes
+  , StandaloneDeriving
+  , DefaultSignatures
+  , FlexibleContexts
+  , DerivingVia
+  , PolyKinds
+  , LambdaCase
+#-}
+
+module Control.Monad.CheckedExcept 
+  ( CheckedExceptT(..)
+  , CheckedExcept
+  , runCheckedExcept
+  , throwCheckedException
+  , OneOf(..)
+  , applyAll
+  , Contains
+  , Elem
+  , weakenExceptions
+  , weakenOneOf
+  , withOneOf
+  ) where
+
+import Data.Functor ((<&>))
+import Control.Exception (Exception(..))
+import Control.Monad.Except
+import Data.Functor.Identity
+import Data.Kind
+import Data.Type.Bool
+import GHC.TypeLits
+import Unsafe.Coerce (unsafeCoerce)
+import Data.Constraint
+import Data.Typeable (Typeable, cast)
+
+newtype CheckedExceptT (exceptions :: [Type]) m a 
+  = CheckedExceptT { runCheckedExceptT :: m (Either (OneOf exceptions) a) }
+  deriving (Monad, Applicative, Functor, MonadFail, MonadIO) via (ExceptT (OneOf exceptions) m)
+  deriving (MonadTrans) via (ExceptT (OneOf exceptions))
+
+type CheckedExcept es a = CheckedExceptT es Identity a
+
+weakenExceptions :: forall exceptions1 exceptions2 m a. 
+     Functor m
+  => Contains exceptions1 exceptions2 
+  => CheckedExceptT exceptions1 m a
+  -> CheckedExceptT exceptions2 m a
+weakenExceptions (CheckedExceptT ma) = CheckedExceptT $ do
+  ma <&> \case
+    Left e -> Left $ (weakenOneOf @exceptions1 @exceptions2 e)
+    Right a -> Right a
+
+weakenOneOf :: forall exceptions1 exceptions2.
+     Contains exceptions1 exceptions2 
+  => OneOf exceptions1
+  -> OneOf exceptions2
+weakenOneOf (OneOf e') = weakenE e'
+  where
+  weakenE :: forall e.
+      ( Elem e exceptions1
+      , CheckedException e 
+      , Typeable e
+      )
+    => e 
+    -> OneOf exceptions2
+  weakenE e = do
+    -- idk how to safely prove this, but the `Contains` constraint guarentees this is true
+    let dict1 :: Dict (Elem e exceptions2)
+        dict1 = unsafeCoerce (Dict @(Elem e exceptions1)) 
+    OneOf e \\ dict1
+
+runCheckedExcept :: CheckedExcept es a -> Either (OneOf es) a
+runCheckedExcept ce = runIdentity (runCheckedExceptT ce)
+
+class Typeable e => CheckedException e where
+  encodeException :: e -> String
+  fromOneOf :: forall es. OneOf es -> Maybe e
+
+  default encodeException :: Exception e => e -> String
+  encodeException = displayException
+
+  default fromOneOf :: Typeable e => OneOf es -> Maybe e
+  fromOneOf (OneOf e) = cast e
+
+newtype ShowException a = ShowException a
+
+instance (Show a, Typeable a) => CheckedException (ShowException a) where
+  encodeException (ShowException x) = show x
+
+deriving via (ShowException ()) instance CheckedException ()
+deriving via (ShowException Int) instance CheckedException Int
+
+data OneOf (es :: [Type]) where 
+  OneOf :: forall e es. (Elem e es, CheckedException e, Typeable e) => !e -> OneOf es
+
+throwCheckedException :: forall e es m a. (Elem e es, CheckedException e, Applicative m) => e -> CheckedExceptT es m a
+throwCheckedException e = do
+  let oneOf :: OneOf es
+      oneOf = OneOf e
+  CheckedExceptT $ pure $ Left oneOf
+
+applyAll :: (forall e. CheckedException e => e -> b) -> OneOf es -> b
+applyAll f (OneOf e) = f e
+
+withOneOf :: (Elem e es, Monoid a, CheckedException e) => OneOf es -> (e -> a) -> a
+withOneOf e f = case fromOneOf e of
+  Just x -> f x
+  Nothing -> mempty
+
+{- 
+type TestExceptions = '[(), Int]
+
+test :: CheckedExcept TestExceptions () -> IO ()
+test ce = case runCheckedExcept ce of
+  Left e -> do 
+    applyAll (putStrLn . encodeException) e
+    -- or
+    withOneOf @() e $ \() -> putStrLn "()"
+    withOneOf @Int e $ \n -> print $ n + 1
+  Right () -> putStrLn "Right"
+
+-- doens't compile
+test2 :: CheckedExcept '[] () -> IO ()
+test2 ce = 
+  case runCheckedExcept ce of
+    Left (OneOf e) -> do 
+      caseException (Proxy @'[]) e Nil
+    Right () -> putStrLn "Right"
+#-}
+
+type family Elem' x xs where
+  Elem' x '[] = 'False
+  Elem' x (x ': xs) = 'True
+  Elem' x (y ': xs) = Elem' x xs
+
+type family Elem x xs :: Constraint where
+  Elem x xs = 
+    If (Elem' x xs) 
+      ('True ~ 'True) 
+      (TypeError ('ShowType x ':<>: 'Text " is not a member of " ':<>: 'ShowType xs))
+
+type family Contains (as :: [k]) (bs :: [k]) :: Constraint where
+  Contains '[] _ = ()
+  Contains (a ': as) bs = (Elem a bs, Contains as bs)
+
---
+++ lib/Control/Monad/CheckedExcept/QualifiedDo.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE
+    KindSignatures
+  , TypeFamilies
+  , DataKinds
+  , TypeOperators
+  , UndecidableInstances
+  , GADTs
+  , TypeApplications
+  , ScopedTypeVariables
+  , RankNTypes
+  , StandaloneDeriving
+  , DefaultSignatures
+  , FlexibleContexts
+  , DerivingVia
+  , PolyKinds
+  , PolyKinds
+  , LambdaCase
+#-}
+
+module Control.Monad.CheckedExcept.QualifiedDo where
+
+import Control.Monad.CheckedExcept 
+import Prelude hiding (Monad(..), Applicative(..))
+import qualified Prelude
+
+(>>=) :: forall exceptions1 exceptions2 exceptions3 m a.
+  ( Contains exceptions1 exceptions3
+  , Contains exceptions2 exceptions3
+  , Prelude.Monad m
+  )
+  => CheckedExceptT exceptions1 m a 
+  -> (a -> CheckedExceptT exceptions2 m a)
+  -> CheckedExceptT exceptions3 m a
+m >>= f = do
+  CheckedExceptT $ do
+    runCheckedExceptT m Prelude.>>= \case
+      Left e -> Prelude.pure $ Left (weakenOneOf @exceptions1 @exceptions3 e)
+      Right a -> runCheckedExceptT (weakenExceptions (f a))
+
+pure :: Prelude.Monad m => a -> CheckedExceptT es m a
+pure = Prelude.pure
+
+return :: Prelude.Monad m => a -> CheckedExceptT es m a
+return = Prelude.return
+
+(>>) :: forall exceptions1 exceptions2 exceptions3 m a x.
+  ( Contains exceptions1 exceptions3
+  , Contains exceptions2 exceptions3
+  , Prelude.Monad m
+  )
+  => CheckedExceptT exceptions1 m x 
+  -> CheckedExceptT exceptions2 m a
+  -> CheckedExceptT exceptions3 m a
+a >> b = weakenExceptions a Prelude.>> weakenExceptions b
+
